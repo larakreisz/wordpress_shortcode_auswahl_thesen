b61e1780a352518503fd0d748aabd3cd478a6523
Lara Kreisz <91203178+larakreisz@users.noreply.github.com>
2023-02-08 14:08:54 +0100

    Update auswahl_thesen_shortcode.php

--- auswahl_thesen_shortcode.php
+++ auswahl_thesen_shortcode.php
@@ -1,1 +1,35 @@
+<?php
 
+/*
+ * The plugin luther_auswahl_thesen
+ * 
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link                https://github.com/larakreisz/wordpress_shortcode_auswahl_thesen
+ * @since               February 8, 2023
+ * @package             Luther Auswahl Thesen
+ * 
+ * @wordpress-plugin
+ * Plugin Name:         Luther Auswahl Thesen
+ * Plugin URI:          https://github.com/larakreisz/wordpress_shortcode_auswahl_thesen
+ * Description:         The plugin generates a shortcode, that provides for each user the relevant luther thesis
+ * Version:             1.0
+ * Author:              Lara Kreisz
+ * Author URI:          https://github.com/larakreisz/wordpress_shortcode_auswahl_thesen
+ */
+if (!defined('WPINC'))
+    die;
+
+/**
+ * Define plugin name to use as global inside the plugin files
+ * Rename this for plugin and update it as you required to change the plugin name for new versions.
+ */
+
+
+
+	
+
+}
